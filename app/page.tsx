cfb7f091777d279812bf3518a4da5613ec0dc449
dooyou123 <deespan@gmail.com>
2025-01-13 23:12:19 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">프레사 인 서울 명동</h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple
-        to use and easy to customize.
+      FRESA INN MYEONGDONG EMPLOYEE HANDBOOK
+      환영합니다! 프레사인 명동 프런트 직원을 위한 위키입니다 ! 🏆
       </p>
       <div className="flex items-center gap-5">
         <Link
           href={`/docs${PageRoutes[0].href}`}
           className={buttonVariants({ className: "px-6", size: "lg" })}
         >
-          Get Started
+          Get Started - Tutorial
         </Link>
       </div>
     </div>
