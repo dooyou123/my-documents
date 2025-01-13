a5769bd54f39a03c9329183978085caf0881e8a0
dooyou123 <deespan@gmail.com>
2025-01-13 23:44:53 +0900

    1

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import { buttonVariants } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
-      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">Fresa Inn Seoul Myeong-Dong</h1>
+      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">Documents</h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-      FRESA INN MYEONGDONG EMPLOYEE HANDBOOK
+        FRESA INN MYEONGDONG EMPLOYEE HANDBOOK
       </p>
       <div className="flex items-center gap-5">
         <Link
--- settings/documents.ts
+++ settings/documents.ts
@@ -2,8 +2,8 @@ import { Paths } from "@/lib/pageroutes"
 
 export const Documents: Paths[] = [
   {
-    title: "Introduction",
-    href: "/introduction",
+    title: "0. Start",
+    href: "/start",
     heading: "Getting started",
     items: [
       {
@@ -23,6 +23,112 @@ export const Documents: Paths[] = [
   {
     spacer: true,
   },
+  {
+    title: "Navigation",
+    href: "/navigation",
+    heading: "Documents",
+  },
+
+  {
+    title: "1. Tutorial",
+    href: "/tutorial",
+    heading: "Tutorial",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    title: "2. About Splaisir",
+    href: "/about",
+    heading: "About Splaisir",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    title: "3. Front Office Information",
+    href: "/info",
+    heading: "Front Office Information",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    title: "4. Wings Sanha IT",
+    href: "/wings",
+    heading: "Wings Sanha IT",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    title: "5. P&P",
+    href: "/p&p",
+    heading: "P&P",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    title: "6. Update Status",
+    href: "/update",
+    heading: "Update Status",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+    ],
+  },
+
+  {
+    spacer: true,
+  },
+
+  {
+    spacer: true,
+  },
+
   {
     title: "Navigation",
     href: "/navigation",
